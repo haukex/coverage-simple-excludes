7e3b6a0efed3dcdc96f9d82f9c6825c7678f16a9
Hauke D <haukex@zero-g.net>
2024-10-11 13:51:11 +0200

    Optimized implementation

--- coverage_simple_excludes/__init__.py
+++ coverage_simple_excludes/__init__.py
@@ -32,45 +32,40 @@ import coverage.plugin
 import coverage.types
 
 # REMEMBER to update README.md when updating the following:
-OS_NAMES :tuple[str,...] = ("posix", "nt", "java")
-SYS_PLATFORMS :tuple[str,...] = ("aix", "emscripten", "linux", "wasi", "win32", "cygwin", "darwin")
-SYS_IMPL_NAMES :tuple[str,...] = ("cpython", "ironpython", "jython", "pypy")
+OS_NAMES :set[str] = { os.name, "posix", "nt", "java" }
+SYS_PLATFORMS :set[str] = { sys.platform, "aix", "emscripten", "linux", "wasi", "win32", "cygwin", "darwin" }
+SYS_IMPL_NAMES :set[str] = { sys.implementation.name, "cpython", "ironpython", "jython", "pypy" }
+_NOTS = { os.name, sys.platform, sys.implementation.name }
+_PREFIX = "#\\s*cover-"
+_OS_PLAT_IMPL = (
+    _PREFIX+'not-(?:'+'|'.join(sorted(sorted( _NOTS ), key=len, reverse=True))+')',
+    _PREFIX+'only-(?:'+'|'.join(sorted(sorted( (OS_NAMES|SYS_PLATFORMS|SYS_IMPL_NAMES) - _NOTS ), key=len, reverse=True))+')',
+)
 
 class MyPlugin(coverage.plugin.CoveragePlugin):
     def __init__(self) -> None:
         pass
     def configure(self, config: coverage.types.TConfigurable) -> None:
         # get config option
-        exclude = config.get_option('report:exclude_lines')
-        #print(f"Before: {exclude!r}", file=sys.stderr)  # Debug
-        if exclude is None:
-            exclude = []
-        assert isinstance(exclude, list)
+        exclude_lines = config.get_option('report:exclude_lines')
+        #print(f"Before: {exclude_lines!r}", file=sys.stderr)  # Debug
+        if isinstance(exclude_lines, list):
+            excludes = exclude_lines
+        # I'm not sure if the following two cases will ever happen
+        elif isinstance(exclude_lines, str):  # pragma: no cover
+            excludes = [exclude_lines]
+        else:  # pragma: no cover
+            excludes = []
         # os / platform / implementation
-        assert os.name in OS_NAMES, f"{os.name=} not in {OS_NAMES=}"
-        assert sys.platform in SYS_PLATFORMS, f"{sys.platform=} not in {SYS_PLATFORMS=}"
-        assert sys.implementation.name in SYS_IMPL_NAMES, f"{sys.implementation.name=} not in {SYS_IMPL_NAMES=}"
-        nots :list[str] = [ os.name, sys.platform, sys.implementation.name ]
-        only :list[str] = (
-            [ x for x in OS_NAMES if x != os.name ] +
-            [ x for x in SYS_PLATFORMS if x != sys.platform ] +
-            [ x for x in SYS_IMPL_NAMES if x != sys.implementation.name ] )
-        nots.sort()
-        nots.sort(key=len, reverse=True)
-        only.sort()
-        only.sort(key=len, reverse=True)
-        exclude.extend( f"#\\s*cover-{e}" for e in (
-            'not-(?:'+'|'.join(nots)+')',
-            'only-(?:'+'|'.join(only)+')',
-            # + python version
-            f"req-lt(?:{re_int_ineq('<=', sys.version_info.major, anchor=False)}\\.[0-9]+"
-            f"|{sys.version_info.major}\\.{re_int_ineq('<=', sys.version_info.minor, anchor=False)})(?![0-9])",
-            f"req-ge(?:{re_int_ineq('>', sys.version_info.major, anchor=False)}\\.[0-9]+"
-            f"|{sys.version_info.major}\\.{re_int_ineq('>',  sys.version_info.minor, anchor=False)})(?![0-9])",
-        ) )
+        excludes.extend(_OS_PLAT_IMPL)
+        # python version
+        excludes.append(f"{_PREFIX}req-lt(?:{re_int_ineq('<=', sys.version_info.major, anchor=False)}\\.[0-9]+"
+            f"|{sys.version_info.major}\\.{re_int_ineq('<=', sys.version_info.minor, anchor=False)})(?![0-9])" )
+        excludes.append(f"{_PREFIX}req-ge(?:{re_int_ineq('>',  sys.version_info.major, anchor=False)}\\.[0-9]+"
+            f"|{sys.version_info.major}\\.{re_int_ineq('>',  sys.version_info.minor, anchor=False)})(?![0-9])" )
         # write config option
         #print(f"After: {exclude!r}", file=sys.stderr)  # Debug
-        config.set_option('report:exclude_lines', exclude)
+        config.set_option('report:exclude_lines', excludes)
 
 def coverage_init(reg :coverage.plugin_support.Plugins, options :dict[str,str]):
     reg.add_configurer(MyPlugin(**options))
